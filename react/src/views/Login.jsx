c3b3b694f3bafc6bc5cf6ff892f6efbfc8807df7
Razvan Melnic <razvan.melnic@gmail.com>
2023-11-12 16:40:56 +0200

    Add login and signup forms

---
+++ react/src/views/Login.jsx
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom";
+
+export default function Login() {
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="login-signup-form animated fadeInDown">
+      <div className="form">
+        <form onSubmit={onSubmit}>
+          <h1 className="title">Login into your account</h1>
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <button type="submit" className="btn btn-block">
+            Login
+          </button>
+          <p className="message">
+            Not Registered? <Link to="/signup">Create and account</Link>
+          </p>
+        </form>
+      </div>
+    </div>
+  );
+}
---
+++ react/src/views/Signup.jsx
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom";
+
+export default function Signup() {
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="login-signup-form animated fadeInDown">
+      <div className="form">
+        <form onSubmit={onSubmit}>
+          <h1 className="title">Signup for free</h1>
+          <input type="text" placeholder="Full Name" />
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <input type="password" placeholder="Password Confirmation" />
+          <button type="submit" className="btn btn-block">
+            Sign up
+          </button>
+          <p className="message">
+            Already Registered? <Link to="/login">Sign in</Link>
+          </p>
+        </form>
+      </div>
+    </div>
+  );
+}
