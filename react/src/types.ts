8b071fb70197e8e757818e79e2676000a683e8ac
Razvan Melnic <razvan.melnic@gmail.com>
2023-11-21 12:54:49 +0200

    Change generic UserType to three more detailed types

--- react/src/types.ts
+++ react/src/types.ts
@@ -4,11 +4,28 @@ export type FormEvent = React.FormEvent<HTMLFormElement>;
 export type MouseEvent = React.MouseEvent<HTMLButtonElement>;
 export type ChangeEvent = React.ChangeEvent<HTMLInputElement>;
 
-export type UserType = {
-  id: number;
+export interface IUserRequest {
   name: string;
   email: string;
-  role: string[];
-  permissions: string[];
+  role?: string[];
+  permissions?: string[];
+  password: string;
+  password_confirmation: string;
+}
+
+export interface IUserResponse {
+  id?: null | never;
+  name: string;
+  email: string;
+  role?: string[];
+  permissions?: string[];
   created_at: string;
-};
+  updated_at: string;
+}
+
+export interface IUsersResponse {
+  status: string;
+  data: {
+    data: IUserResponse[];
+  };
+}
