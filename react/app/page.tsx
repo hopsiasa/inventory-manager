4a6450c74f877e70c5dea439578de78dd4a39d0b
Razvan Melnic <razvan.melnic@gmail.com>
2023-11-27 16:30:38 +0200

    NextJS initial commit

---
+++ react/app/page.tsx
@@ -0,0 +1,43 @@
+import { Card, Text, Title } from '@tremor/react';
+import { sql } from '@vercel/postgres';
+import Search from './search';
+import UsersTable from './table';
+
+interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+}
+
+export default async function IndexPage({
+  searchParams
+}: {
+  searchParams: { q: string };
+}) {
+  const search = searchParams.q ?? '';
+  // const result = await sql`
+  //   SELECT id, name, username, email
+  //   FROM users
+  //   WHERE name ILIKE ${'%' + search + '%'};
+  // `;
+  const users = [
+    {
+      id: 1,
+      name: 'John',
+      email: 'john@example.com',
+      username: 'John'
+    }
+  ] as User[];
+
+  return (
+    <main className="p-4 md:p-10 mx-auto max-w-7xl">
+      <Title>Users</Title>
+      <Text>A list of users retrieved from a Postgres database.</Text>
+      <Search />
+      <Card className="mt-6">
+        <UsersTable users={users} />
+      </Card>
+    </main>
+  );
+}
