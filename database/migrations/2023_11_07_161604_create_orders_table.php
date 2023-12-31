5b8dab3c7673489ee09ae7b9a2ec9c042ffa63fe
Razvan Melnic <razvan.melnic@gmail.com>
2023-12-31 14:46:04 +0200

    Add unique() to order_id column

--- database/migrations/2023_11_07_161604_create_orders_table.php
+++ database/migrations/2023_11_07_161604_create_orders_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->string('order_id');
+            $table->string('order_id')->unique();
             $table->foreignId('product_id')->constrained()->cascadeOnDelete();
             $table->string('customer')->nullable();
             $table->integer('quantity');
